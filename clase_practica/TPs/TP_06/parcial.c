a2d5f923ab31e426340c2da37eb3dd1eafab68c1
Lucas Talamo <lutalamo@itba.edu.ar>
2026-06-20 23:07:26 -0300

    Cambios Linux

--- clase_practica/TPs/TP_06/parcial.c
+++ clase_practica/TPs/TP_06/parcial.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <ctype.h>
+#include "../../random.h"
 #include <stdio.h>
 #include <string.h>
 
@@ -315,4 +316,266 @@ int sonAmigas (unsigned cols, const int m1[][cols], unsigned fil1, const int m2[
   if (matrizAmiga(cols, m1, m2, fil1, fil2)) return 1;
   if (matrizAmiga(cols, m2, m1, fil2, fil1)) return 2;
   return 0;
+}
+
+// ejercicio 20 - sudoku - ejercicio tipo parcial
+#define DIM 9
+#define VALID_NUM(a) ((a >= 1 && a <= 9)? 1 : 0)
+int validarFila (char v[]){
+  int aparece[9] = {0};
+  int i = 0;
+  for (; i < DIM; i++){
+    if (!VALID_NUM(v[i])) return 0;
+    if (!aparece[v[i] - 1]){
+      aparece[v[i] - 1] = 1;
+    }
+    else return 0;
+  }
+  return 1;
+}
+
+int validarColumna (char cuadrado[][DIM], int col){
+  int aparece[9] = {0};
+  int i = 0;
+  for (; i < DIM; i++){
+    if (!VALID_NUM(cuadrado[i][col])) return 0;
+    if (!aparece[cuadrado[i][col] - 1]){
+      aparece[cuadrado[i][col] - 1] = 1;
+    }
+    else return 0;
+  }
+  return 1;
+}
+
+int validarSubCuadrado (char cuadrado[][DIM], int i, int idx_j){
+  int aparece[9] = {0};
+  int dim_i = i + 3;
+  int dim_j = idx_j + 3;
+  for (; i < dim_i; i++){
+    for (int j = idx_j; j < dim_j; j++){
+      if (!VALID_NUM(cuadrado[i][j])) return 0;
+      if (!aparece[cuadrado[i][j] - 1]){
+        aparece[cuadrado[i][j] - 1] = 1;
+      }
+      else return 0;
+    }
+  }
+  return 1;
+}
+
+int validarSudoku (char m[][DIM]){
+  for (int i = 0; i < DIM; i++){
+    if (!validarColumna(m, i) || !validarFila(m[i])) return 0;
+    
+    if (i % 3 == 0){
+      for (int j = 0; j < DIM; j += 3){
+        if (!validarSubCuadrado(m, i, j)) return 0;
+      }
+  }
+  }
+  return 1;
+}
+
+//ejercicio 24
+void eliminaBlancos (char s[]){
+  int j = 0;
+  for (int i = 0; s[i] != '\0'; i++){
+    if (s[i] != ' '){
+      s[j++] = s[i];
+    }
+    else if (s[i] == ' ' && s[i+1] != ' '){
+      s[j++] =s[i];
+    }
+  }
+  s[j] = '\0';
+}
+
+// ejercicio 3 - parcial 2023 1C
+#define COL 5
+#define FIL 6
+// retorna 0 si va gris, 1 verde, 2 amarillo
+int pertenece (char c, const char * oculta, int pos){
+  if (oculta[pos] == c) return 1;
+
+  for (int i = 0; i < COL; i++){
+    if (oculta[i] == c) return 2;
+  }
+  return 0;
+}
+
+int wordle (const char *oculta, const char m[FIL][COL], char ans[][COL], int cant_int){
+  for (int i = 0; i < cant_int; i++){
+    //contador de letras correctas
+    int contador = 0;
+
+    for (int j = 0; j < COL; j++){
+      int color = pertenece(m[i][j], oculta, j);
+      if (color == 0){
+        ans[i][j] = 'G';
+      }
+      else if (color == 1){
+        ans[i][j] = 'V';
+        contador++;
+      }
+      else if (color == 2){
+        ans[i][j] = 'A';
+      }
+    }
+    // retorno i+1, que es en el intento que se logro 
+    if (contador == COL) return ++i;
+  }
+  // si sale es porque ninguno fue correcto
+  return -1;
+}
+
+// ejercicio 2 - parcial 2023 1C
+#define K 6
+#define VALID(a) ((a) >= 1 && (a) <= 3*K)
+
+// retorna la suma, o -1 si se repite algun numero o es invalido
+int subCuadrado (const int matriz[][K], int pos_i, int pos_j){
+  int aparece[K*3] = {0};
+  int suma = 0;
+  for (int i = pos_i; i < pos_i+3; i++){
+    for (int j = pos_j; j < pos_j+3; j++){
+      if (VALID(matriz[i][j])){
+        if (!aparece[matriz[i][j] - 1]){
+          aparece[matriz[i][j] - 1] = 1;
+          suma += matriz[i][j];
+        }
+        else return -1;
+      }
+      //si es un numero invalido
+      else return -1;
+    }
+  }
+  // si llega hasta aca, no se repite nada, y los valores son correctos
+  return suma;
+}
+
+int verifica (const int matriz[][K]){
+  int suma = 0;
+  for (int i = 0; i < K; i += 3){
+    for (int j = 0; j < K; j += 3){
+      int actual = subCuadrado(matriz, i, j);
+      if (suma == 0){
+        suma = actual;
+      }
+      if (actual == -1) return 0;
+      if (suma != actual){
+        return 0;
+      }
+    }
+  }
+  return 1;
+}
+
+// ejercicio 1 - parcial 2023 1C
+void elimina (char *s1, const char *s2, const char *s3){
+  int j = 0;
+  int s2_fin = 0;
+  int s3_fin = 0;
+  for (int i = 0; s1[i] != '\0'; i++){
+    if (!s2_fin && s2[i] == '\0') s2_fin = 1;
+    if (!s3_fin && s3[i] == '\0') s3_fin = 1;
+    
+    int sacar = 0;
+    if (!s2_fin && s1[i] == s2[i]) sacar = 1;
+    if (!s3_fin && s1[i] == s3[i]) sacar = 1;
+    if (!sacar) s1[j++] = s1[i];
+  }
+  s1[j] = '\0';
+}
+
+// ejercicio 1 - recu 2022 1C
+
+char teclado[10][26] = { 
+  {0}, {0},
+  {'A', 'B', 'C'},       // 2
+  {'D', 'E', 'F'},       // 3
+  {'G', 'H', 'I'},       // 4
+  {'J', 'K', 'L'},       // 5
+  {'M', 'N', 'O'},       // 6
+  {'P', 'Q', 'R', 'S'},  // 7
+  {'T', 'U', 'V'},       // 8
+  {'W', 'X', 'Y', 'Z'}   // 9
+};
+
+void convertirATexto (unsigned long num, char * texto){
+  if (num == 0) {
+    *texto = '0';
+    texto[1] = '\0';
+    return;
+  }
+  int len = 0;
+  unsigned long n = num;
+  while (n != 0){
+    n /= 10;
+    len++;
+  }
+  //reseto n
+  n = num;
+
+  for (int i = len-1; i >= 0; i--){
+    int last = n % 10;
+    n /= 10;
+    if (last == 0 || last == 1){
+      texto[i] = '0' + last;
+    }
+    else if (last == 7 || last == 9){
+      // obtengo la letra random
+      int random = randInt(0, 3);
+      texto[i] = teclado[last][random];
+    }
+    else {
+      int random = randInt(0, 2);
+      texto[i] = teclado[last][random];
+    }
+  }
+  texto[len] = '\0';
+}
+
+// ejercicio 3 - recu 2022 1C
+#define D 4
+#define VALIDAR(a) ((a) >= 1 && (a) <= N*N)
+int sumaCol (int matriz[][N], int col){
+  int suma = 0;
+  for (int i = 0; i < N; i++){
+    if (VALIDAR(matriz[i][col])){
+      suma += matriz[i][col];
+    }
+    else return 0;
+  }
+  return suma;
+}
+
+int sumaFil (int matriz[][N], int fil){
+  int suma = 0;
+  for (int i = 0; i < N; i++){
+    if (VALIDAR(matriz[fil][i])){
+      suma += matriz[fil][i];
+    }
+    else return 0;
+  }
+  return suma;
+}
+
+int esMagico (int matriz[][N]){
+  int aparece[N*N] = {0};
+  int suma = 0;
+  for (int i = 0; i < N; i++){
+    int sumActual = sumaFil(matriz, i);
+    if (sumActual == 0) return 0;
+    if (suma == 0) suma = sumActual;
+    if (suma != sumActual) return 0;
+    for (int j = 0; j < N; j++){
+      sumActual = sumaCol(matriz, j);
+      if (suma != sumActual) return 0;
+      if (!aparece[matriz[i][j] - 1]){
+        aparece[matriz[i][j] - 1] = 1;
+      }
+      else return 0;
+    }
+  }
+  return 1;
 }
